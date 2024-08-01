afa9ea20c14e6af51bcb49b66b6bd058e10ec9e6
GitHubMaria87092 <mariakhanofficial78@gmail.com>
2024-08-01 03:22:22 -0700

    6th js assingment

---
+++ app.js
@@ -0,0 +1,74 @@
+// question 1
+
+// var num = +prompt("Enter Positive Integers Number");
+// var round = Math.round(num);
+// var floor = Math.floor(num);
+// var ceil = Math.ceil(num);
+// document.write("Number:" + " " + num + "<br>" + "Round off Value:" + " " + round + "<br>" + "Floor Value:" + " " + floor + "<br>" + "Ceil Value:" + " " + ceil + "<br>")
+
+// question 2
+
+// var num1 = +prompt("Enter Negative floating point");
+// var round = Math.round(num1);
+// var floor = Math.floor(num1);
+// var ceil = Math.ceil(num1);
+// document.write("Number:" + " " + num1 + "<br>" + "Round off Value:" + " " + round + "<br>" + "Floor Value:" + " " + floor + "<br>" + "Ceil Value:" + " " + ceil + "<br>")
+
+// question 3
+
+// var num = +prompt("Enter any Number");
+// var absoluteValue;
+// if(num<0){
+//     absoluteValue = -num; 
+// }
+// else{
+//     absoluteValue = num;
+// }
+// document.write("The absolute value of" + " " + num + " " + "is" + " " + absoluteValue + "<br>")
+
+// question 4
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+// document.write("Dice Value is" + " " + dice);
+
+// question 5
+
+// var heads = prompt("Enter heads Name");
+// var tails = prompt("Enter tails Name");
+// var toss = Math.floor(Math.random()*2);
+
+// if(toss == 1){
+//     alert(heads + " " + "is win")
+// }
+// else{
+//     alert(tails + " " + "is win")
+// }
+
+// question 6
+
+// var ran = Math.floor(Math.random() * 100) + 1;
+// document.write("Random Number between 1 to 100 is" + " " + ran + "<br>")
+
+// question 7
+
+// var weight = prompt("Enter Your weight");
+// var float = parseFloat(weight);
+
+// if(!isNaN(weight)){
+//     document.write("Your Weight is" + " " + weight);
+// }
+// else{
+//     document.write("Invalid, Please Enter valid weight")
+// }
+
+// question 8
+
+// var secNum = 8;
+// var userInput = +prompt("Enter Any Number Between 1 to 10");
+
+// if(userInput === secNum){
+//     alert("You Win")
+// }
+// else{
+//     alert("Better Luck Next time")
+// }
